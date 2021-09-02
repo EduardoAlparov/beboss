8d5d83c22833d3352616f2043f7df8258eccc18e
Eduard <workman133@yandex.ru>
2021-09-02 18:14:24 +0300

    first commit

---
+++ #src/js/app.js
@@ -0,0 +1,1 @@
+@@include('files/video-slider.js', {})
---
+++ #src/js/files/menu.js
@@ -0,0 +1,29 @@
+$(function () {
+  $(document).ready( function () {
+    $('.js-head-btn').on('click', function () {
+      $('.js-menu-container').addClass('active');
+      $('body').addClass('disable');
+      $('.js-menu-btn').addClass('close');
+    })
+  });
+
+  $(document).ready( function () {
+    $('.js-menu-btn').on('click', function () {
+      $('.js-menu-container').removeClass('active');
+      $('body').removeClass('disable');
+      $('.js-menu-btn').removeClass('close');
+    })
+  });
+
+  $(document).mouseup(function (e) {
+    var container = $(".js-menu-container");
+    if (!container.is(e.target) && container.has(e.target).length === 0 && container.hasClass('active')) {
+      container.removeClass('active');
+      $('body').removeClass('disable');
+      $('.js-menu-btn').removeClass('close');
+    }
+  });
+});
+(function () {
+  alert('Я функция, и я запустилась.');
+  }());
